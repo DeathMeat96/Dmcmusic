c2ace468c2d94d7f6a8241a9d8231ae2c043e8d0
DeathMeat96 <dinca.marius96@gmail.com>
2024-02-11 13:33:30 +0200

    yrdy yrdy xxxx sdsdsd send

---
+++ pages/index.js
@@ -0,0 +1,58 @@
+
+// next image
+
+import Image from "next/image";
+
+
+// components
+
+import ParticlesContainer from '../components/ParticlesContainer'
+import ProjectsBtn from '../components/ProjectsBtn'
+import Avatar from '../components/Avatar'
+
+// framer motion
+
+import {motion} from 'framer-motion'
+
+//variants
+
+import {fadeIn} from '../variants'
+
+const Home = () => {
+  return <div className='bg-primary-/60 h-full'>
+    {/* text */}
+    <div className='w-full h-full bg-gradient-to-r from-primary/10 via-black/70 to-black/10'>
+      <div className='text-center flex flex-col justify-center xl:pt-40 xl:text-left h-full  container mx-auto xl:mix-blend-color-dodge xl:max-w-[700px] absolute xl:left-[250px] md:mix-blend-normal md:max-w-[1200px] sm:left-[10px]  ' >
+    {/* title */}
+      <h1 className="h1 bg-black/40 rounded-xl">
+      Explorați universul muzicii noastre pline de pasiune,<br/> perfectă pentru a creea {''}
+        <span className='text-accent'>o atmosferă unică și memorabilă.</span>
+      </h1>
+    {/* Subtitle */}
+      <p className='max-w-sm xl:max-w-xl mx-auto xl:mx-0 mb-10 xl:mb-16 bg-black/40 rounded-xl'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque nostrum quam reprehenderit vero, tenetur voluptatem nulla aut aspernatur dolores ut.
+      </p>
+    {/* Btn */}
+      <div className='flex justify-center xl:hidden relative'>
+        <ProjectsBtn />
+      </div>
+      <motion.div 
+      variants={fadeIn ('down', 0.4)}
+      initial='hidden'
+      animate='show'
+      exit='hidden'
+      className='hidden xl:flex'
+      >
+        <ProjectsBtn />
+      </motion.div>
+    </div>
+  </div>
+  {/*image */}
+    <div className="w-full h-full absolute right-0 bottom-0 xl:mix-blend-soft-light md:mix-blend-soft-light sm:mix-blend-soft-light">
+      
+  {/*BG */}
+  <div className='bg-none xl:bg-explosion xl:bg-cover xl:bg-right xl:bg-no-repeat w-full h-full absolute translate-z-0 lg:bg-explosion lg:bg-cover lg:bg-right lg:bg-no-repeat md:bg-explosion md:bg-cover md:bg-right sm:bg-no-repeat sm:bg-explosion sm:bg-cover sm:bg-right  sm:bg-no-repeat'></div>
+    </div>
+</div>;
+};
+
+export default Home;
