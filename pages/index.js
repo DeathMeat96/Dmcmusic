681a33db296403831dc9bfa152f82098955b9f04
DeathMeat96 <dinca.marius96@gmail.com>
2024-02-21 09:16:49 +0200

    U5

--- pages/index.js
+++ pages/index.js
@@ -25,8 +25,8 @@ const Home = () => {
       <div className='text-center flex flex-col justify-center xl:pt-40 xl:text-left h-full  container mx-auto ' >
     {/* title */}
       <h1 className="h1 bg-black/40 rounded-xl md:text-xl sm:text-base xs:text-base ">
-      Explorați universul muzicii noastre pline de pasiune,<br/> perfectă pentru a creea {''}
-        <span className='text-accent'>o atmosferă unică și memorabilă.</span>
+      Explorați universul muzicii noastre pline de pasiune,<br/> perfectă pentru a creea o {''}
+        <span className='text-accent'>atmosferă unică și memorabilă.</span>
       </h1>
     {/* Subtitle */}
       <p className='max-w-sm xl:max-w-xl mx-auto xl:mx-0 mb-10 xl:mb-16 bg-black/40 rounded-xl xs:hidden'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque nostrum quam reprehenderit vero, tenetur voluptatem nulla aut aspernatur dolores ut.
@@ -50,7 +50,7 @@ const Home = () => {
     <div className="w-full h-full absolute right-0 bottom-0 xl:mix-blend-soft-light lg:mix-blend-soft-light md:mix-blend-soft-light sm:mix-blend-soft-light xs:mix-blend-soft-light z-10">
       
   {/*BG */}
-  <div className='bg-none xl:bg-explosion xl:bg-cover xl:bg-right xl:bg-no-repeat w-full h-full absolute translate-z-0 lg:bg-explosion lg:bg-cover lg:bg-right lg:bg-no-repeat md:bg-explosion md:bg-cover md:bg-right sm:bg-no-repeat sm:bg-explosion sm:bg-cover sm:bg-right  sm:bg-no-repeat xs:bg-no-repeat xs:bg-explosion xs:bg-cover xs:bg-right  xs:bg-no-repeat'></div>
+  <div className='bg-none xl:bg-explosion xl:bg-cover xl:bg-right xl:bg-no-repeat w-full h-full absolute translate-z-0 lg:bg-explosion lg:bg-cover lg:bg-right lg:bg-no-repeat md:bg-explosion md:bg-cover md:bg-right sm:bg-no-repeat sm:bg-explosion sm:bg-cover sm:bg-right   xs:bg-explosion xs:bg-cover xs:bg-right  xs:bg-no-repeat'></div>
     </div>
 </div>;
 };
